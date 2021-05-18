ea3a603f7c5e96b3a3182862e5c1c407292a2ae0
Nikolaus J. Reichert <nikolaus@njreichert.ca>
2021-05-18 10:16:17 -0500

    Added card and deck definitions.

---
+++ deck.h
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+typedef enum {
+    DIAMONDS,
+    HEARTS,
+    SPADES,
+    CLUBS
+} SUIT;
+
+typedef char Rank;
+
+/**
+ * @brief A Deck. Effectively a linked list of Cards with extra metadata. 
+ */
+typedef struct deck {
+    Card top; /**< The linked list of cards. */
+    size_t size; /**< Must be changed manually. */
+};
+
+/**
+ * @brief A Card. A linked list node with data for suit and rank. 
+ */
+typedef struct Card {
+    SUIT suit;
+    Rank rank;
+    Card *next; /**< The pointer to the next card. */
+};
