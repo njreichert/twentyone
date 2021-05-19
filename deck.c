f4fccccb146e268636ee10f993bbb0a15b3b9265
Nikolaus J. Reichert <nikolaus@njreichert.ca>
2021-05-18 19:40:03 -0500

    Added basic deck-handling functions.

---
+++ deck.c
@@ -0,0 +1,149 @@
+/* Included via deck.h: <stdlib.h> */
+#include <assert.h>
+#include <stdio.h>
+#include "deck.h"
+
+const char suitChars[] = "dhsc";
+const char rankChars[] = "AxxxxxxxxxJQK"; /* Ace, placeholders, Jack, Queen, King. */
+
+Deck * initDeck(int fillCards)
+{
+    Deck * newDeck = malloc(sizeof(Deck));
+
+    newDeck->size = 0;
+
+    if (fillCards) {
+        for (size_t currentSuit = 0; currentSuit < NUM_SUITS; currentSuit++) {
+            for (size_t currentRank = 0; currentRank < NUM_RANKS; currentRank++) {
+                Card * newCard = initCard(currentSuit, currentRank);
+                pushCard(newDeck, newCard);
+            } 
+        }
+    }
+
+    return newDeck;
+}
+
+
+void deinitDeck(Deck * deck)
+{
+    while (deck->size > 0) {
+        Card * removedCard = popCard(deck);
+        deinitCard(removedCard);
+    }
+}
+
+
+Card * popCard(Deck * deck)
+{
+    return removeCard(deck, 0);
+}
+
+
+void pushCard(Deck * deck, Card * cardToAdd)
+{
+    addCard(deck, cardToAdd, 0);
+}
+
+
+void addCard(Deck * deck, Card * cardToAdd, size_t pos)
+{
+    size_t currentIndex = 0;
+    Card * currentCard = deck->top;
+    Card * previousCard = NULL;
+
+    assert(pos <= deck->size);
+
+    while (currentIndex < pos) {
+        previousCard = currentCard;
+        currentCard = currentCard->next;
+    }
+
+    if (previousCard != NULL) {
+        previousCard->next = cardToAdd;
+    } else { /* Implies the first index of the deck. */
+        deck->top = cardToAdd;
+    }
+    cardToAdd->next = currentCard;
+
+    deck->size++;
+}
+
+
+Card * removeCard(Deck * deck, size_t pos)
+{
+    size_t currentIndex = 0;
+    Card * currentCard = deck->top;
+    Card * previousCard = NULL;
+
+    while (currentCard != NULL && currentIndex < pos) {
+        previousCard = currentCard;
+        currentCard = currentCard->next;
+        currentIndex++;
+    }
+
+    if (currentCard != NULL) {
+        if (previousCard != NULL) {
+            previousCard->next = currentCard->next;
+        } else {
+            deck->top = currentCard->next;
+        }
+    }
+
+    currentCard->next = NULL;
+
+    deck->size--;
+
+    return currentCard;
+}
+
+
+Card * initCard(Suit suit, Rank rank)
+{
+    Card * newCard = malloc(sizeof(Card));
+
+    newCard->suit = suit;
+    newCard->rank = rank;
+
+    return newCard;
+}
+
+
+void deinitCard(Card * card)
+{
+    free(card);
+}
+
+
+void printDeck(Deck * deck, int withBraces)
+{
+    if (withBraces) printf("<< ");
+
+    size_t currentIndex = 0;
+    Card * currentCard = deck->top;
+
+    while (currentIndex < deck->size) {
+
+        char currentRankChar = rankChars[currentCard->rank];
+        char currentSuitChar = suitChars[currentCard->suit];
+
+        /* 
+         * Certain card types (face cards, etc) need special treatment.
+         * this is determined by the placeholder value of 'x'.
+         */
+        if (currentRankChar != 'x') {
+            printf("%c%c", currentRankChar, currentSuitChar);
+        } else {
+            printf("%u%c", currentCard->rank + 1, currentSuitChar);
+        }
+
+        /* Add a space only if the current card is not the last. */
+        if (currentIndex < deck->size - 1) printf(" ");
+
+        currentCard = currentCard->next;
+        currentIndex++;
+        
+    }
+
+    if (withBraces) printf(" >>");
+}
--- deck.h
+++ deck.h
@@ -3,30 +3,116 @@
 
 #include <stdlib.h>
 
-typedef enum SUIT {
-    DIAMONDS,
-    HEARTS,
-    SPADES,
-    CLUBS
-} SUIT;
+#define NUM_RANKS 13
+#define NUM_SUITS 4
 
-typedef char Rank;
-
-/**
- * @brief A Deck. Effectively a linked list of Cards with extra metadata. 
+/* 
+ * Suit definitions. Easier to iterate through so long as the values
+ * Are well-defined. 
  */
-typedef struct deck {
-    Card top; /**< The linked list of cards. */
-    size_t size; /**< Must be changed manually. */
-};
+#define DIAMONDS 0
+#define HEARTS 1
+#define SPADES 2
+#define CLUBS 3
+
+typedef unsigned char Suit;
+typedef unsigned char Rank;
 
 /**
  * @brief A Card. A linked list node with data for suit and rank. 
  */
 typedef struct Card {
-    SUIT suit;
+    Suit suit;
     Rank rank;
-    Card *next; /**< The pointer to the next card. */
-};
+    struct Card * next; /**< The pointer to the next card. */
+} Card;
+
+/**
+ * @brief A Deck. Effectively a linked list of Cards with extra metadata. 
+ */
+typedef struct Deck {
+    Card * top; /**< The linked list of cards. */
+    size_t size; /**< Must be changed manually. */
+} Deck;
+
+/* Function Definitions */
+
+
+/**
+ * @brief Initializes a new deck.
+ * 
+ * @param fillCards will fill the deck with (NUM_RANKS * NUM_SUITS) Cards.
+ * @returns a pointer to the new deck.
+ */
+Deck * initDeck(int fillCards);
+
+/**
+ * @brief Cleans up and frees memory associated with a deck.
+ * 
+ * @param deck The deck in question.
+ */
+void deinitDeck(Deck * deck);
+
+/**
+ * @brief An alias for removeCard() at position 0.
+ * 
+ * @param deck The deck in question.
+ * @return The card removed.
+ */
+Card * popCard(Deck * deck);
+
+/**
+ * @brief A wrapper for addCard(deck, 0).
+ * 
+ * @param deck The deck in question.
+ * @param cardToAdd The card to add.
+ */
+void pushCard(Deck * deck, Card * cardToAdd);
+
+/**
+ * @brief Adds a card at a given position.
+ * 
+ * This function checks by assertion that the position is within the bounds of the deck. 
+ * 
+ * @param deck The deck in question.
+ * @param cardToAdd The card in question.
+ * @param pos The position to add a card to.
+ */
+void addCard(Deck * deck, Card * cardToAdd, size_t pos);
+
+/**
+ * @brief Removes the card at a given position.
+ * 
+ * @param deck The deck in question.
+ * @param pos The position to remove.
+ * @return A pointer to the card removed with its next member set to NULL. Returns NULL if the index is out of bounds.
+ */
+Card * removeCard(Deck * deck, size_t pos);
+
+/**
+ * @brief Creates a card with a rank and suit.
+ * 
+ * Users of this function must modify Card.next to their liking.
+ * 
+ * @param suit 
+ * @param rank 
+ * @return a pointer to the new card.
+ */
+Card * initCard(Suit suit, Rank rank);
+
+/**
+ * @brief A wrapper to free(card). Frees the memory associated with the card.
+ * 
+ * @param card The card in question.
+ */
+void deinitCard(Card * card);
+
+/**
+ * @brief Prints a representation of the deck.
+ * 
+ * @param deck The deck in question.
+ * @param withBraces will add braces to the beginning and end of the string.
+ */
+void printDeck(Deck * deck, int withBraces);
 
 #endif
