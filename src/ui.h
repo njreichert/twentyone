377ac0eab44c11cec06210f1586f67a5d8ba31da
Nikolaus J. Reichert <nikolaus@njreichert.ca>
2021-05-25 12:21:32 -0500

    Rename Element. Add Terminal structure.

--- src/ui.h
+++ src/ui.h
@@ -3,6 +3,8 @@
 
 #include <curses.h>
 
+#define MAX_WINDOWS 8
+
 typedef enum ElementType {
     GENERIC,
     INPUTBOX,
@@ -10,13 +12,26 @@ typedef enum ElementType {
 } ElementType;
 
 /* A window in curses, with extra metadata to aid in redrawing if need be. */
-typedef struct Element {
+typedef struct WindowWrapper {
     WINDOW * win;
     int y; /** Initial vertical location. Zero-indexed. */
     int x; /** Initial horizontal location. Zero-indexed. */
     int rows;
     int cols;
     int priority; /** Determines order at which Elements will be rendered. */
-} Element;
+} WindowWrapper;
+
+/* 
+ * The terminal as a whole. Contains a list of MAX_ELEMENTS elements and metadata
+ * relating to the terminal.
+ */
+typedef struct Terminal {
+    int rows;
+    int cols;
+    int isCBreak;
+    int isNoEcho;
+    int isKeypad;
+    WindowWrapper * wins[MAX_WINDOWS];
+} Terminal;
 
 #endif
