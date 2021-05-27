6ae6af92e8fde847437590f26b44cc6740427fb1
Nikolaus J. Reichert <nikolaus@njreichert.ca>
2021-05-26 23:29:50 -0500

    Add test routine for rendering.

--- src/main.c
+++ src/main.c
@@ -2,19 +2,38 @@
 #include <stdio.h>
 
 #include "deck.h"
+#include "ui.h"
+#include "player.h"
 
 int main(void)
 {
-    Deck * deckOne = initDeck(1);
 
-    printf("Preshuffle: ");
-    printDeck(deckOne, 0);
-    printf("\n");
+    getchar();
 
-    shuffleDeck(deckOne);
-    printf("PostShuffle: ");
-    printDeck(deckOne, 1);
-    printf("\n");
+    Player * players[6] = {0};
+
+    for (size_t i = 0; i < MAX_PLAYERS; i++) {
+        players[i] = initPlayer(100);
+    }
+
+    ScreenWrapper * sc = initScreenWrapper();
+    initUI(sc);
+
+    for (int i = 0; i < sc->numWindows; i++) {
+        wborder(sc->wins[i], 0, 0, 0, 0, 0, 0, 0, 0);
+    }
+
+    refreshAll(sc);
+    for (size_t i = 0; i < sc->numWindows; i++)
+    {
+        mvwprintw(sc->wins[0], i + 1, 1, "window %d: %d\t%d", i, getmaxy(sc->wins[i]), getmaxx(sc->wins[i]));
+        refreshAll(sc);
+    }
+
+    deinitScreenWrapper(sc);
+
+
+    getchar();
 
     
 
