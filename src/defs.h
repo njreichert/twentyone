049cfad7d8b0fc35b28a9f1bee7ebd62993b704c
Nikolaus J. Reichert <nikolaus@njreichert.ca>
2021-05-30 22:53:06 -0500

    Add various defs.

--- src/defs.h
+++ src/defs.h
@@ -1,9 +1,17 @@
 #ifndef DEFS_H
 #define DEFS_H
 
+/* PLAYER RELATED DEFS */
+#define NAME_LEN 32
 #define MAX_PLAYERS 4
+#define STARTING_BAL 100
+
+/* WINDOW AND IO RELATED DEFS */
 #define NUM_OPTIONS 2
 #define MAX_OPTION_SIZE 8
 #define BUF_LEN 128
+
+/* STRING LITERALS */
+#define STR_NUM_PLAYERS "How many players? (Max 4) "
 
 #endif
