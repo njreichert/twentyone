7d279b8fdcbee72faebd553df99638560e0e120a
Nikolaus J. Reichert <nikolaus@njreichert.ca>
2021-06-13 21:06:45 -0500

    Modify buffer length for efficiency

--- src/defs.h
+++ src/defs.h
@@ -9,7 +9,10 @@
 /* WINDOW AND IO RELATED DEFS */
 #define NUM_OPTIONS 2
 #define MAX_OPTION_SIZE 8
-#define BUF_LEN 128
+#define BUF_LEN 32
+
+/* CARD RELATED DEFS */
+#define MAX_CARD_STR 4
 
 /* STRING LITERALS */
 #define STR_NUM_PLAYERS "How many players? (Max 4) "
