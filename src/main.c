3f82bb133aa8b73a255d6c0326ad8aece94f2c61
Nikolaus J. Reichert <nikolaus@njreichert.ca>
2021-08-23 17:26:41 -0500

    Add testcase.

--- src/main.c
+++ src/main.c
@@ -13,12 +13,22 @@ int main(void)
 
     Player * players[6] = {0};
     Player * dealer = {0};
+    Deck * deck = initDeck(1);
+
+    int i = 0;
+
+    char pname[NAME_LEN + 1];
 
     for (size_t i = 0; i < MAX_PLAYERS; i++) {
-        players[i] = initPlayer(100);
+        snprintf(pname, NAME_LEN, "player%lu", i);
+        players[i] = initPlayer(100, pname);
+        players[i]->status = NO_STATUS;
     }
 
-    char buf[256] = "";
+    players[2]->status = CUR_PLAYER;
+
+    snprintf(pname, NAME_LEN, "player%d", i);
+    dealer = initPlayer(1000, pname);
 
     ScreenWrapper * sc = initScreenWrapper();
     initUI(sc);
@@ -27,29 +37,14 @@ int main(void)
 
     drawScreen(sc, players, dealer);
 
-    mvwprintw(sc->dealerWin, 1, 1, "Dealer Window: %d\t%d", getmaxy(sc->dealerWin), getmaxx(sc->dealerWin));
-    refreshAll(sc);
-    
-    for (size_t i = 0; i < MAX_PLAYERS; i++)
+    for (size_t i = 0; i < 20; i++)
     {
-        mvwprintw(sc->dealerWin, i + 2, 1, "Player %d: %d\t%d", i + 1, getmaxy(sc->playerWins[i]), getmaxx(sc->playerWins[i]));
-        refreshAll(sc);
+        dealCard(deck, players[0]->hand, 1);
+        drawScreen(sc, players, dealer);
     }
-
-    const char * opt[2] = {"Option A", "Opt B"};
-
-    mvwprintw(sc->inputWin, MAX_PLAYERS + 2, 1, "Input Window: %d\t%d", getmaxy(sc->inputWin), getmaxx(sc->inputWin));
-    refreshAll(sc);
-    // char c = getInput(sc->inputWin, "test test test test test test test test test", opt, "ab");
-
-    size_t a = getString(sc, buf, 15, "What is your name? ");
-
-    printCentred(sc->playerWins[2], 3, "test 123");
+    
 
     deinitScreenWrapper(sc);
-
-    
-    printf("%lu, %s\n", a, buf);
 
     getchar();
 
